1e076e43f5654935b86af3c9fe151bfd7d52b92a
gavin.lin <gavinlin@gmail.com>
2015-12-05 00:01:12 +0800

    修改項目1203 ~ part 1

--- js/donation_drift_btn.js
+++ js/donation_drift_btn.js
@@ -4,11 +4,8 @@ $(function(){
         var top = $(this).scrollTop();
         if(top!=0){
             var top_px = $(this).scrollTop() + 100;
-            $('.wf-donation-drift-btn').animate({top:top_px}, 200);
+            //$('.wf-donation-drift-btn').animate({top:top_px}, 200);
+            $('.wf-donation-drift-btn').css("top",top_px + "px");
         }
-        
-        /*var top_px = $(this).scrollTop() + 100;
-        $('.wf-donation-drift-btn').animate({scrollTop:top_px}, '500');
-        $('.wf-donation-drift-btn').css("top","px");*/
     });
 });
